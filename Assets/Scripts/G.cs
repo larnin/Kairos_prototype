0822acd805fc7a1ffd8ef0cf38740062fecd9a81
Nicolas <nicolas-donald.laurent@laposte.net>
2017-10-12 11:17:45 +0200

    Create base project
    G, NRand, Events, DoTween

---
+++ Assets/Scripts/G.cs
@@ -0,0 +1,15 @@
+﻿
+public sealed class G
+{
+    private static volatile G m_instance;
+
+    public static G sys
+    {
+        get
+        {
+            if (G.m_instance == null)
+                G.m_instance = new G();
+            return G.m_instance;
+        }
+    }
+}
