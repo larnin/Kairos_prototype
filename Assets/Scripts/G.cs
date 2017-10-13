adce619de464d2b3a5d301e81824fbd16ad08d4d
Unknown <juliensud@live.fr>
2017-10-13 17:22:58 +0200

    Add the CursorLogic for minigame

--- Assets/Scripts/G.cs
+++ Assets/Scripts/G.cs
@@ -1,7 +1,11 @@
 ﻿
+using UnityEngine;
+
 public sealed class G
 {
     private static volatile G m_instance;
+    private ExampleEventAndRandom m_test = null;
+
 
     public static G sys
     {
@@ -10,6 +14,21 @@ public sealed class G
             if (G.m_instance == null)
                 G.m_instance = new G();
             return G.m_instance;
+        }
+    }
+
+    public ExampleEventAndRandom test
+    {
+        get
+        {
+            return m_test;
+        }
+
+        set
+        {
+            if (test != null)
+                Debug.LogError("Test instancied 2 times");
+            test = value;
         }
     }
 }
---
+++ Assets/Scripts/MiniGames/CursorLogic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorLogic : MonoBehaviour {
+
+    [SerializeField]
+    private float m_speed = 1000f;
+    [SerializeField]
+    private int borderOffset = 25;
+
+    private Camera m_camera;
+    private const string m_horizontalInputName = "CursorHorizontal";
+    private const string m_VerticalInputName = "CursorVertical";
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_camera = Camera.main; 
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float HorizontalInputValue = Input.GetAxis(m_horizontalInputName);
+        float VerticalInputValue = Input.GetAxis(m_VerticalInputName);
+        
+        if (HorizontalInputValue != 0 || VerticalInputValue != 0)
+        {
+            // move the cursor along the screen even if it's a 3D object.
+            Vector3 currentScreenPosition = m_camera.WorldToScreenPoint(transform.position);
+            Vector3 newScreenPosition = currentScreenPosition + (new Vector3(HorizontalInputValue, -VerticalInputValue)) * m_speed * Time.deltaTime;
+
+            newScreenPosition.x = Mathf.Clamp(newScreenPosition.x, 0f + borderOffset, Screen.width - borderOffset);
+            newScreenPosition.y = Mathf.Clamp(newScreenPosition.y, 0f + borderOffset, Screen.height - borderOffset);
+
+            transform.position = m_camera.ScreenToWorldPoint(newScreenPosition);
+        }   
+	}
+}
