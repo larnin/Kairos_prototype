33fa32c9c25bb25550f09cfe0af76e631b37af69
Unknown <juliensud@live.fr>
2017-10-19 11:26:26 +0200

    WIP on PhraseToFind

---
+++ Assets/PhrasesToFind.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhrasesToFind : MonoBehaviour {
+
+
+    //private string[]
+
+    private List<string> selectedPhrases = new List<string>();
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        selectedPhrases.Clear();
+    }
+
+    public void SelectPhrase(string phrase)
+    {
+
+    }
+
+    public void UnSelectPhrase(string phrase)
+    {
+        selectedPhrases.Remove(phrase);
+    }
+}
